8072022b56911389477dcc5767aadec7a1d66eb1
qqjq547 <qqjq547@gmail.com>
2019-03-21 13:49:09 +0800

    修改扫码页面

--- app/src/main/java/com/guochuang/mimedia/ui/activity/common/MyCaptureActivity.java
+++ app/src/main/java/com/guochuang/mimedia/ui/activity/common/MyCaptureActivity.java
@@ -1,15 +1,25 @@
 package com.guochuang.mimedia.ui.activity.common;
 
+import android.Manifest;
+import android.app.AppComponentFactory;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.donkingliang.imageselector.utils.ImageSelector;
 import com.guochuang.mimedia.base.BaseActivity;
+import com.guochuang.mimedia.base.BasePresenter;
+import com.guochuang.mimedia.base.MvpActivity;
+import com.guochuang.mimedia.tools.Constant;
+import com.guochuang.mimedia.ui.activity.redbag.EditRedbagActivity;
 import com.sz.gcyh.KSHongBao.R;
+import com.tbruyelle.rxpermissions.RxPermissions;
 import com.uuzuche.lib_zxing.activity.CaptureActivity;
 import com.uuzuche.lib_zxing.activity.CaptureFragment;
 import com.uuzuche.lib_zxing.activity.CodeUtils;
@@ -17,15 +27,19 @@ import com.uuzuche.lib_zxing.activity.CodeUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.functions.Action1;
 
-public class MyCaptureActivity extends BaseActivity {
+public class MyCaptureActivity extends MvpActivity {
     @BindView(R.id.iv_back)
     ImageView ivBack;
     @BindView(R.id.tv_title)
     TextView tvTitle;
     @BindView(R.id.fl_content)
     FrameLayout flContent;
-
+@Override
+protected BasePresenter createPresenter() {
+    return null;
+}
     @Override
     public int getLayout() {
         return R.layout.activity_my_capture;
@@ -34,13 +48,22 @@ public class MyCaptureActivity extends BaseActivity {
     @Override
     public void initViewAndData() {
         tvTitle.setText(R.string.scan_pay);
-
-        CaptureFragment captureFragment = new CaptureFragment();
-        //定制化扫描框UI
-        CodeUtils.setFragmentArgs(captureFragment,R.layout.view_qrcode_scan);
-        //分析结果回调
-        captureFragment.setAnalyzeCallback(analyzeCallback);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fl_content,captureFragment).commit();
+        RxPermissions rxPermissions=new RxPermissions(this);
+        rxPermissions.request(Manifest.permission.CAMERA).subscribe(new Action1<Boolean>() {
+            @Override
+            public void call(Boolean aBoolean) {
+                if (aBoolean) {
+                    CaptureFragment captureFragment = new CaptureFragment();
+                    //定制化扫描框UI
+                    CodeUtils.setFragmentArgs(captureFragment,R.layout.view_qrcode_scan);
+                    //分析结果回调
+                    captureFragment.setAnalyzeCallback(analyzeCallback);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fl_content,captureFragment).commit();
+                }else {
+                    showShortToast(R.string.get_camera_permission);
+                }
+            }
+        });
     }
 
     @OnClick(R.id.iv_back)
@@ -73,4 +96,6 @@ public class MyCaptureActivity extends BaseActivity {
             finish();
         }
     };
+
+
 }
